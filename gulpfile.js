9ad637306f774d1da4168403d281ccc8762be9c7
Thomas Haukland <thomas.haukland@gmail.com>
2018-02-25 21:54:47 +0100

    PDF version

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp'),
+    replace = require('gulp-string-replace'),
     hb = require('gulp-hb');
 
 gulp.task('default', function () {
@@ -9,5 +10,19 @@ gulp.task('default', function () {
             helpers: './src/*.js',
             partials: './src/*.hbs'
         }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('pdf', function () {
+      return gulp
+        .src('./src/cv.tex')
+        .pipe(hb({
+            data: './src/*.json',
+            helpers: './src/*.js',
+            partials: './src/*.hbs'
+        }))
+        .pipe(replace(/#/g, '\\#'))
+        .pipe(replace(/.png/g, ''))
+        .pipe(replace(/\&quot;/g, '"'))
         .pipe(gulp.dest('./'));
 });
